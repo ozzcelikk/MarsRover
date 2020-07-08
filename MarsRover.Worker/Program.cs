757c32f8b4ed41062544e6e0a27966a210e11b82
kadir.ozcelik@flypgs.com <kadir.ozcelik@flypgs.com>
2020-07-08 12:45:14 +0300

    program edited

--- MarsRover.Worker/Program.cs
+++ MarsRover.Worker/Program.cs
@@ -1,10 +1,10 @@
-﻿using MarsRover.Contracts.Models;
+﻿using MarsRover.Contracts.Extensions;
+using MarsRover.Contracts.Models;
 using MarsRover.Contracts.Services;
 using MarsRover.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using MarsRover.Contracts.Extensions;
 
 namespace MarsRover.Worker
 {
@@ -17,7 +17,11 @@ namespace MarsRover.Worker
 
         #endregion
 
-        public static int MaxRoverCount => 1;
+        public static int MaxRoverCount => 4;
+
+        public static Plateau CurrentPlateau { get; set; }
+
+        public static List<Rover> RoverList { get; set; }
 
         static void Main(string[] args)
         {
@@ -29,102 +33,109 @@ namespace MarsRover.Worker
                     .BuildServiceProvider();
 
             _plateauService = serviceProvider.GetService<IPlateauService>();
-            _roverService = serviceProvider.GetService<IRoverService>(); 
+            _roverService = serviceProvider.GetService<IRoverService>();
 
             #endregion
 
+            RoverList = new List<Rover>();
+
             Console.WriteLine("Create a new plateau");
-
-            Plateau plateau = null;
-
-            var rovers = new List<Rover>();
 
             var work = true;
 
             while (work)
             {
-                #region Plateau
+                PlateauWorker();
 
-                while (plateau == null)
-                {
-                    var plateauCoordinateString = Console.ReadLine();
+                RoverWorker();
 
-                    plateau = _plateauService.CreatePlateau(plateauCoordinateString);
-
-                    if (plateau == null)
-                    {
-                        Console.WriteLine("Please enter valid coordinate values like this template: \"3 3\"");
-                    }
-                }
-
-                #endregion
-
-                #region Rover
-
-                while (rovers.Count != MaxRoverCount)
-                {
-                    var roverIsReady = false;
-                    var roverCommandsAreReady = false;
-
-                    Console.WriteLine($"Enter {rovers.Count + 1}.Rover coordinate");
-
-                    while (!roverIsReady)
-                    {
-                        var roverCoordinateString = Console.ReadLine();
-
-                        var currentRover = _roverService.CreateRover(roverCoordinateString, plateau);
-
-                        if (currentRover == null)
-                        {
-                            Console.WriteLine($"Please enter {rovers.Count + 1}.Rover valid values like this template: \"1 2 N\"");
-                        }
-                        else
-                        {
-                            roverIsReady = true;
-
-                            Console.WriteLine($"Enter {rovers.Count + 1}.Rover directives");
-
-                            while (!roverCommandsAreReady)
-                            {
-                                var roverDirectiveString = Console.ReadLine();
-
-                                var roverCommands = _roverService.GetCommands(roverDirectiveString);
-
-                                if (roverCommands.Length == 0)
-                                {
-                                    Console.WriteLine($"Please enter {rovers.Count + 1}.Rover valid command values like this template: \"MLRRMLMM\"");
-                                }
-                                else
-                                {
-                                    roverCommandsAreReady = true;
-
-                                    currentRover.Commands = roverCommands;
-
-                                    rovers.Add(currentRover);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                #endregion
-
-                var count = 0;
-
-                foreach (var rover in rovers)
-                {
-                    count++;
-
-                    _roverService.ExecuteCommands(rover);
-
-                    Console.WriteLine($"{count}. Rover => {rover.Location.X} {rover.Location.Y} {rover.Heading.GetCode()}");
-                }
+                ExecuteWorker();
 
                 Console.WriteLine("Press any key for exit");
 
                 Console.ReadLine();
 
                 work = false;
+            }
+        }
+
+        public static void PlateauWorker()
+        {
+            while (CurrentPlateau == null)
+            {
+                var plateauCoordinateString = Console.ReadLine();
+
+                CurrentPlateau = _plateauService.CreatePlateau(plateauCoordinateString);
+
+                if (CurrentPlateau == null)
+                {
+                    Console.WriteLine("Please enter valid coordinate values like this template: \"3 3\"");
+                }
+            }
+        }
+
+        public static void RoverWorker()
+        {
+            while (RoverList.Count != MaxRoverCount)
+            {
+                var roverIsReady = false;
+                var roverCommandsAreReady = false;
+
+                Console.WriteLine($"Enter {RoverList.Count + 1}.Rover coordinate");
+
+                while (!roverIsReady)
+                {
+                    var roverCoordinateString = Console.ReadLine();
+
+                    var roverCreateResult = _roverService.CreateRover(roverCoordinateString, CurrentPlateau);
+
+                    var currentRover = roverCreateResult.Rover;
+
+                    if (currentRover == null)
+                    {
+                        Console.WriteLine(roverCreateResult.Message);
+                    }
+                    else
+                    {
+                        roverIsReady = true;
+
+                        Console.WriteLine($"Enter {RoverList.Count + 1}.Rover directives");
+
+                        while (!roverCommandsAreReady)
+                        {
+                            var roverDirectiveString = Console.ReadLine();
+
+                            var roverCommands = _roverService.GetCommands(roverDirectiveString);
+
+                            if (roverCommands.Length == 0)
+                            {
+                                Console.WriteLine($"Please enter {RoverList.Count + 1}.Rover valid command values like this template: \"MLRRMLMM\"");
+                            }
+                            else
+                            {
+                                roverCommandsAreReady = true;
+
+                                currentRover.Commands = roverCommands;
+
+                                RoverList.Add(currentRover);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void ExecuteWorker()
+        {
+            var count = 0;
+
+            foreach (var rover in RoverList)
+            {
+                count++;
+
+                _roverService.ExecuteCommands(rover);
+
+                Console.WriteLine($"{count}. Rover => {rover.Location.X} {rover.Location.Y} {rover.Location.Heading.GetCode()}");
             }
         }
     }
